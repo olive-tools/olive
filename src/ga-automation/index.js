a895bcff08407a6202dc26c7f84928584ae329ff
Arthur Frade de Araujo <arthur4raujo@gmail.com>
2023-09-18 21:56:26 -0300

    fix: auth ga-automation

--- src/ga-automation/index.js
+++ src/ga-automation/index.js
@@ -1,3 +1,19 @@
+const { isValidAuth } = require('../auth');
+
 async function formSubmitHandler(event) {
-    console.log('event', event);
+    if (!isValidAuth(event.headers.authorization)) {
+        return {
+            statusCode: 401,
+        };
+    }
+    const row = JSON.parse(event.body).event.values;
+    return {
+        statusCode: 200,
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify(row)
+    };
 }
+
+module.exports = { formSubmitHandler };
