c07d3100f40988b2e0a4a4c9f266341d6ad5f7c7
Arthur Frade de Araujo <arthur4raujo@gmail.com>
2024-03-06 09:50:49 -0300

    feat: typescript setup and types

---
+++ src/types/address.ts
@@ -0,0 +1,8 @@
+export class Address {
+    constructor(
+        readonly street: string,
+        readonly city: string,
+        readonly state: string,
+        readonly cep: string
+    ) { }
+}
