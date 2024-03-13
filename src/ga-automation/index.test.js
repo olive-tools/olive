e498514a2795363929b86e346bc154fa51b64c11
Arthur Frade de Araujo <arthur4raujo@gmail.com>
2024-03-13 13:38:24 -0300

    test: should update new insurance sheet columns

--- src/ga-automation/index.test.js
+++ src/ga-automation/index.test.js
@@ -2,26 +2,26 @@ const resultsFromQuery = [{
     customer: {
         M: {
             name: {
-                S: ''
+                S: 'name1'
             },
             cpf: {
-                S: ''
+                S: 'cpf1'
             },
             birth: {
-                S: ''
+                S: 'birth1'
             }
         }
     },
     passenger: {
         M: {
             name: {
-                S: ''
+                S: 'name2'
             },
             cpf: {
-                S: ''
+                S: 'cpf2'
             },
             birth: {
-                S: ''
+                S: 'birth2'
             }
         }
     }
@@ -65,10 +65,18 @@ describe('insuranceScheduleHandler', () => {
         await insuranceScheduleHandler({}, new Date('2024-03-11T09:00:00.000Z'));
         expect(dynamoDbAdapter.getByPK).toHaveBeenCalledWith('TOURS_TABLE', { tourDate: '2024-03-11' });
     });
-    
+
     test("should create a copy of base insurance sheet", async () => {
         await insuranceScheduleHandler({}, new Date('2024-03-11T09:00:00.000Z'));
         expect(copyFile).toHaveBeenCalledWith('INSURANCE_FILE_ID', '2024-03-11-seguros');
+    });
+
+    test("should update new insurance sheet columns", async () => {
+        await insuranceScheduleHandler({}, new Date('2024-03-11T09:00:00.000Z'));
+        expect(insertSheetRows).toHaveBeenCalledWith('COPYED_SHEET_ID', 'Plan1!B8:K9', [
+            ['name1', 'cpf1', 'birth1', '2024-03-11', '2024-03-11', 30000, '', '', 1, 1],
+            ['name2', 'cpf2', 'birth2', '2024-03-11', '2024-03-11', 30000, '', '', 1, 1]
+        ]);
     });
 });
 
